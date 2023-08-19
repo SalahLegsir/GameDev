261c11c9023a2feec61946de328ee349d55906da
SalahLegsir <s129055@ap.be>
2023-08-19 16:46:56 +0200

    Fix bug

--- MyGame/Won.cs
+++ MyGame/Won.cs
@@ -19,7 +19,7 @@ namespace MyGame
         public Won(SpriteBatch spriteBatch, Texture2D restartTexture, Texture2D homeTexture, Texture2D nextTexture)
         {
             _spriteBatch = spriteBatch;
-            RestartButton = new Button(restartTexture, new Vector2(340, 500), new Hitbox(320, 500, restartTexture.Width, restartTexture.Height));
+            RestartButton = new Button(restartTexture, new Vector2(340, 500), new Hitbox(340, 500, restartTexture.Width, restartTexture.Height));
             NextButton = new Button(nextTexture, new Vector2(455, 500), new Hitbox(455, 500, nextTexture.Width, nextTexture.Height));
             HomeButton = new Button(homeTexture, new Vector2(455, 500), new Hitbox(455, 500, homeTexture.Width, homeTexture.Height));
 
